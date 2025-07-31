82d38df20caec7e5e5e60583a2acb218a76faa7f
Wm. Breck Seiniger <157913805+WmBreck@users.noreply.github.com>
2025-07-31 17:03:34 -0400

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { HeroComponent } from './app/components/hero.component';
 import { ServicesComponent } from './app/components/services.component';
-import { GalleryComponent } from './app/components/gallery.component';
-import { TestimonialsComponent } from './app/components/testimonials.component';
 import { ContactComponent } from './app/components/contact.component';
 import { FooterComponent } from './app/components/footer.component';
 import { StickyCTAComponent } from './app/components/sticky-cta.component';
@@ -14,8 +12,6 @@ import { StickyCTAComponent } from './app/components/sticky-cta.component';
   imports: [
     HeroComponent,
     ServicesComponent,
-    GalleryComponent,
-    TestimonialsComponent,
     ContactComponent,
     FooterComponent,
     StickyCTAComponent
@@ -24,8 +20,6 @@ import { StickyCTAComponent } from './app/components/sticky-cta.component';
     <div class="app">
       <app-hero></app-hero>
       <app-services></app-services>
-      <app-gallery></app-gallery>
-      <app-testimonials></app-testimonials>
       <app-contact></app-contact>
       <app-footer></app-footer>
       <app-sticky-cta></app-sticky-cta>
